23b0ab3052697f3a7e2c989702f3c760323ce174
Dmitry Korytsev <dkorytsev@gmail.com>
2014-11-26 13:34:20 +0300

    Added some macros for Semaphores without counters (mutex) each semaphore require 1 byte RAM. for calling need to place addres of semaphore to Z register

--- SemMutex.asm
+++ SemMutex.asm
@@ -1,92 +1,72 @@
-; В IPC kordos входят следующие вызовы
-
-; IPC_Send    - посылка сообщения
-; IPC_Recv    - получение сообщения
-; IPC_SemUp   - Проверка и установка семафора
-; IPC_SemDown - Снятие семафора
-
-
-; Семафоры
-; Количество семафоров жёстко задано в коде. никаких вызовов для их создания
-
-; Структура для хранения семафоров
-; 1 байт для хранения значения семафора
-; 1 байт для хранения максимального значения семафора
 
 .MACRO CALL_SemUp   
     CLI
-    PUSH R16
-    LDI  R16, @0
-    CALL IPC_SemUp
-    POP  R16
+    PUSH ZL
+    PUSH ZH
+    LDI  ZL , low(@0)
+    LDI  ZH , high(@0)
+    CALL IPC_SemByte_Up
+    POP  ZH
+    POP  ZL
+    SEI
 .ENDM
 
 .MACRO CALL_SemDown
     CLI
-    PUSH R16
-    LDI  R16, @0
+    PUSH ZL
+    PUSH ZH
+    LDI  ZL , low(@0)
+    LDI  ZH , high(@0)
     CALL IPC_SemDown
-    POP  R16
+    POP  ZH
+    POP  ZL
+    SEI
 .ENDM
 
 
-.MACRO IPC_SEMUP
-IPC_SemUp:                    ; Preconditions: Номер семафора хранится в R16
+.MACRO IPC_semByte_Up
+.DEF temp = R16
+IPC_semByte_Up:                    
     CLI
-    PUSH ZL                   ; расчищаем регистры для работы
-    PUSH ZH
+    PUSH temp
+
+IPC_semByte_Up_check:
+    LD    temp , Z                 ; Загрузили значение семафора
+    CPI   temp , 0                 ; Проверили значение семафора 0 - опущен 1- взведён
+    BREQ  IPC_semByte_Up_set       ; Если семафор опущен то взводим его
+    RJMP  TaskBreak                ; Если семафор поднят то ждём когда он опустится, отдаём управление ядру
+    RJMP  IPC_semByte_Up_check     ; После возврата из ядра идём опять в проверку семафора
+
+IPC_semByte_Up_set:
+    SBR   temp                      
+    ST    Z  , temp
     
-    LDIZ  Semaphores          ; Загружаем семафор, берём адрес начала области памяти с семафорами и прибавляем смещение из R16 
-    ADD   ZL  , R16
-    LDI   R16 , 0
-    ADC   ZH  , R16
-
-IPC_semup_load_and_test:
-    LD    R16 , Z                  ; Загрузили значение семафора
-    CPI   R16 , 0                  ; Проверили значение семафора 0 - опущен 1- взведён
-    BREQ  IPC_semup_set            ; Если семафор не взведён  то прыгаем на установку семафора
-    TaskBreak                      ; Если взведён то отдаем вожжи ядру
-    RJMP  IPC_semup_load_and_test  ; После возврата из ядра идём опять в проверку семафора
-
-IPC_semup_set:
-    SBR   R16                      ; Если семафор не равен максимальному уровню то есть меньше его ( не вижу причин для его превышения) то инкрементим его и RETI
-    ST    Z  , R16                 ; сохраняем значение семафора обратно в память
-    
-    POP ZH
-    POP ZL
-
-IPC_SemUpReti:    
+    POP temp
     RETI
+.UNDEF temp
 .ENDM
 
-.MACRO IPC_SEMDOWN
-IPC_SemDown:                  ; Preconditions: Номер семафора хранится в R16
-    CLI
-    PUSH ZL                   ; расчищаем регистры для работы
-    PUSH ZH
-        
-    LDIZ  Semaphores          ; Загружаем семафор, берём адрес начала области памяти с семафорами и прибавляем смещение из R16 
-    ADD   ZL  , R16
-    LDI   R16 , 0
-    ADC   ZH  , R16
-    
-    LD    R16 , Z+            ; Значение семафора
-    CPI   R16 , 0
-    BREQ  IPC_SemDownReti     ; Если семафор уже взведён до максимального уровня  то прерываем задачу и отдаём управление ядру
-    DEC   R16                 ; Если семафор не равен максимальному уровню то есть меньше его ( не вижу причин для его превышения) то инкрементим его и RETI
-    
-    SUBI  ZL                  ; смещаем адрес в регистре Z на 1 байт назад, чтобы он показывал на байт со значением семафора
-    LDI   R17, 0
-    SBCI  ZH , R17
-    
-    ST    Z  , R16            ; сохраняем значение семафора обратно в память
-    
-    POP R17                   ; Восстанавливаем регистры
-    POP ZH
-    POP ZL
 
-IPC_SemDownReti:    
+.MACRO IPC_semByte_Down
+.DEF temp = R16
+IPC_semByte_Down:                    
+    CLI
+    PUSH temp
+
+IPC_semByte_Down_check:
+    LD    temp , Z                  ; Загрузили значение семафора
+    CPI   temp , 0                  ; Проверили значение семафора 0 - опущен 1- взведён
+    BRNE  IPC_semByte_Down_set      ; Если семафор поднят то опускаем его
+    RJMP  IPC_semByte_Down_return   ; Если семафор уже опущен, то возврат
+
+IPC_semByte_Down_set:
+    CLR   temp                      
+    ST    Z  , temp
+    
+IPC_semByte_Down_return:
+    POP temp
     RETI
+.UNDEF temp
 .ENDM
 
 
